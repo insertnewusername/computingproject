9c6f85dc79bf5eab6af56f522877d6e30374aba4
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-25 09:06:53 +1000

    use css classes

--- theme.js
+++ theme.js
@@ -1,17 +1,24 @@
 function toggletheme() {
-    var element = document.getElementById("body");
+
+    var element = document.body;
     element.classList.toggle("bodydark");
 
+    /* var element = document.getElementById("body");
+    element.classList.toggle("bodydark"); */
+
+    var element = document.getElementsByClassName("normalcontainer");
+    element.classList.toggle("normalcontainerdark");
 
     var element = document.getElementById("featuredprojects");
     element.classList.toggle("ourgamesdark");
 
-    /* Selects and toggles all normalcontainer divs */
+    /* Selects and toggles all normalcontainer divs 
     var items = document.querySelectorAll(".normalcontainer");
 
     for (var i = 0; i < items.length; i++) {
      items[i].classList.toggle("normalcontainerdark");
     }
+     */
 
 
 }
