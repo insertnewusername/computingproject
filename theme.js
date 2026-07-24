f31fdea9f94a98dda5bd46eddad56534a7673365
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-24 21:34:04 +1000

    make toggledark toggle more stuff

--- theme.js
+++ theme.js
@@ -1,4 +1,17 @@
 function toggletheme() {
     var element = document.getElementById("body");
     element.classList.toggle("bodydark");
+
+
+    var element = document.getElementById("featuredprojects");
+    element.classList.toggle("ourgamesdark");
+
+    /* Selects and toggles all normalcontainer divs */
+    var items = document.querySelectorAll(".normalcontainer");
+
+    for (var i = 0; i < items.length; i++) {
+     items[i].classList.toggle("normalcontainerdark");
+    }
+
+
 }
