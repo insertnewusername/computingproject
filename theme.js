810f199f430f163cf4fe114f30fe956f4f96cf38
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-07-25 14:49:38 +1000

    Make icons svg Co-authored-by: jishnusrinath-dev <jishnusrinath-dev@users.noreply.github.com>

--- theme.js
+++ theme.js
@@ -4,3 +4,8 @@ document.getElementById('toggler').addEventListener('change', (event) => {
   event.target.checked ? body.setAttribute('data-theme', 'dark') /* If checked, set datatheme to dark */ : body.removeAttribute('data-theme'); /* if unchecked, remove dark datatheme (changes to light default) */
 });
 
+function toggled()
+{
+var eleement = document.getElementById("toggleicon")
+element.src = "images/Sun.svg"
+}
